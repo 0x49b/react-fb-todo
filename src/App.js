3b157aa2ea3006518ef86f014f11fea4cae8b61c
Florian Thiévent <florianthievent@MBP-von-Florian.fritz.box>
2022-10-30 21:03:45 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {TextField , Button } from '@mui/material';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [todos,setTodos]=useState([
+'Create Blockchain App',
+'Create a Youtube Tutorial'
+]);
+const [input, setInput]=useState('');
+const addTodo=(e)=>{
+e.preventDefault();
+setTodos([...todos,input]);
+setInput('')
+};
+return (
+<div className="App">
+<h2> TODO List App</h2>
+<form>
+<TextField id="outlined-basic" label="Make Todo" variant="outlined" style={{margin:"0px 5px"}} size="small" value={input}
+onChange={e=>setInput(e.target.value)} />
+<Button variant="contained" color="primary" onClick={addTodo}  >Add Todo</Button>
+</form>
+<ul>
+{todos.map(todo => <li>{todo}</li>)}
+</ul>
+</div>
+);
 }
-
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlT_oTzAmlssSOPEpkoG2QUBmzz5-deUg",
+  authDomain: "todo-app-57e0f.firebaseapp.com",
+  projectId: "todo-app-57e0f",
+  storageBucket: "todo-app-57e0f.appspot.com",
+  messagingSenderId: "1022317589234",
+  appId: "1:1022317589234:web:73186609c5a345c5c3566b"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+export { db }
